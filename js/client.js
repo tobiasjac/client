aaf7e42a42170da8bbabfc40ebde7dfc1bee0a87
Tobias Jacobsen <tobias_jac@hotmail.com>
2015-11-18 21:57:26 +0100

    Trying to create a function that allows the user to delete a game

--- js/client.js
+++ js/client.js
@@ -17,7 +17,10 @@ $(document).ready(function() {
         };
 
         $.ajax(settings).done(function (response) {
-            window.location.href="../html/UserMenu.html";
+                window.location.href="../html/UserMenu.html";
+                if (xhr.status == 400) {
+                    console.log("something went wrong");
+                }
         });
     });
 });
@@ -39,6 +42,23 @@ $(document).ready(function() {
                     trHTML += '<tr><td>' + item.id + '</td><td>' + item.firstName + '</td><td>' +item.lastName + '</td><td>' + item.email + '</td><td>'+ item.username + '</td><td>'+ item.created + '</td></tr>';
                 });
                 $('#table').append(trHTML);
+            console.log(response);
+        });
+    });
+});
+
+//method that deletes a game in the database according to gameId specified by user
+$(document).ready(function() {
+    $("#gameid").click(function () {
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://localhost:10018/api/games/",
+            "method": "DELETE"
+        };
+
+        $.ajax(settings.url + $("#id").val()).done(function (response) {
             console.log(response);
         });
     });
